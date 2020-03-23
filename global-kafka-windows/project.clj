357d834e8d077dcff92c8cfc5840ffba794156c5
mn-dimension <matthew.newland@gmail.com>
2020-03-23 15:05:25 +0000

    updated clojure to 1.10 and system java

--- global-kafka-windows/project.clj
+++ global-kafka-windows/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.onyxplatform/onyx-kafka "0.14.5.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
                  [org.onyxplatform/onyx "0.14.5"]]
